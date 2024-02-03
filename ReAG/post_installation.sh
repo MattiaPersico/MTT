14807cb0b9fecf64357cf977a3559c92a7ec7e02
Mattia Persico <mattiapersico@Mattias-MBP.lan>
2024-02-03 17:28:07 +0100

    update installer e post install script

--- ReAG/post_installation.sh
+++ ReAG/post_installation.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+# Imposta la variabile REAG_DIR con il percorso della cartella target
 REAG_DIR="/Applications/ReAG_Environment"
 
-# Ottieni il nome utente
+# Ottieni il nome utente dell'utente attuale
 CURRENT_USER=$(whoami)
 
-# Cambia il proprietario di REAG_DIR
+# Cambia il proprietario di REAG_DIR e di tutti i suoi contenuti all'utente attuale
 chown -R "$CURRENT_USER" "$REAG_DIR"
 
-# u+rwx: Imposta i permessi di lettura, scrittura ed esecuzione per l'utente
-chmod -R u+rwx "$REAG_DIR"
+# Imposta i permessi completi per il proprietario, il gruppo e gli altri
+chmod -R 777 "$REAG_DIR"
 
-# Aggiungi il percorso REAG_DIR e i suoi contenuti all'elenco delle applicazioni consentite
-spctl --add --recursive "$REAG_DIR"
+# Rimuovi le eventuali ACL esistenti
+chmod -R -N "$REAG_DIR"
 
-# Attiva l'ambiente
+# Verifica e modifica le ACL per dare pieni permessi a tutti
+# Questo comando imposta l'ACL per garantire pieni permessi agli utenti 'everyone'
+chmod +a "everyone allow read,write,execute,delete,append,file_inherit,directory_inherit" "$REAG_DIR"
+
+# Attiva l'ambiente specificato
 source /Applications/ReAG_Environment/AG_P3Env_02/bin/activate
